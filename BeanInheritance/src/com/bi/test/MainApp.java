177677bff6b28c2191b0e34f74a4bfa5a5cfef9e
PawanSingh51093 <47606743+PawanSingh51093@users.noreply.github.com>
2019-02-13 22:56:05 +0530

    Add files via upload
    bean inheritance

---
+++ BeanInheritance/src/com/bi/test/MainApp.java
@@ -0,0 +1,23 @@
+package com.bi.test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bi.beans.HelloIndia;
+import com.bi.beans.HelloWorld;
+
+public class MainApp {
+   private static ApplicationContext context;
+
+public static void main(String[] args) {
+      context = new ClassPathXmlApplicationContext("Beans.xml");
+      
+      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+      objA.getMessage1();
+      objA.getMessage2();
+
+      HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
+      objB.getMessage1();
+      objB.getMessage2();
+      objB.getMessage3();
+   }
+}
